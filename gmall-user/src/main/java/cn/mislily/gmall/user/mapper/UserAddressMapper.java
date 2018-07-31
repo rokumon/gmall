c52db3a1da46ee6a8cf19f9eb72e80c48100963a
rokumon <rokumon@163.com>
2018-07-31 20:11:34 +0800

    分布式项目搭建 完成基础测试

---
+++ gmall-user/src/main/java/cn/mislily/gmall/user/mapper/UserAddressMapper.java
@@ -0,0 +1,7 @@
+package cn.mislily.gmall.user.mapper;
+
+import cn.mislily.gmall.bean.UserAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserAddressMapper extends Mapper<UserAddress> {
+}
--- gmall-user/src/main/java/cn/mislily/gmall/user/mapper/UserInfoMapper.java
+++ gmall-user/src/main/java/cn/mislily/gmall/user/mapper/UserInfoMapper.java
@@ -1,10 +1,9 @@
 package cn.mislily.gmall.user.mapper;
 
-import cn.mislily.gmall.user.bean.UserInfo;
+import cn.mislily.gmall.bean.UserInfo;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface UserInfoMapper extends Mapper<UserInfo> {
-
 
 
 }
