779212c0705aaa6497a8e18728ea730441fb3f82
rokumon <rokumon@163.com>
2018-08-09 15:48:49 +0800

    创建 list 模块

---
+++ gmall-list-web/src/main/java/cn/mislily/gmall/list/controller/ListController.java
@@ -0,0 +1,19 @@
+package cn.mislily.gmall.list.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ListController {
+
+    @RequestMapping("list")
+    public String search(){
+        return "list";
+    }
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+}
